f242705a69004a9b4a687e8e738efd945864a10b
Austin <aus10b@gmail.com>
2019-04-05 19:54:25 -0500

    initial commit; completed exercise requirements

---
+++ Methods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VertaforeChallenges
+{
+    public class Methods
+    {
+        public static void Pause()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static int[] Randomize(int[] NumberList)
+        {
+            Random rnd = new Random();
+            int[] MyRandomArray = NumberList.OrderBy(x => rnd.Next()).ToArray();
+            return MyRandomArray;
+        }
+
+        public static string Abbreviate(string GivenWord)
+        {
+            char GWFirstCharacter = GivenWord[0];
+            char GWLastCharacter = GivenWord[GivenWord.Length - 1];
+            int RemovedCount = GivenWord.Count() - 2;
+            string AbbreviatedString = $"{GWFirstCharacter}{RemovedCount}{GWLastCharacter}";
+            return AbbreviatedString;
+         }
+
+        public static void CheckUniqueness(string wtc, Dictionary<string, string> waa)
+        {
+            Dictionary<string, bool> WordsAndUniqueness = new Dictionary<string, bool>();
+            string WordToCompare = wtc;
+            foreach (KeyValuePair<string, string> kvp in waa)
+            {
+                bool AbIsUnique = true;
+                if (Abbreviate(WordToCompare) == kvp.Value)
+                {
+                    AbIsUnique = false;
+                }
+                WordsAndUniqueness.Add(kvp.Key, AbIsUnique);
+                ;
+            }
+            if (WordsAndUniqueness.ContainsValue(false))
+            {
+                Console.WriteLine($"The abbreviation of {WordToCompare} is not unique.");
+            }
+            else
+            {
+                Console.WriteLine($"The abbreviation of {WordToCompare} '{Abbreviate(WordToCompare)}' is unique.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VertaforeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Question 1
+            Write an application that satisfies the following:
+            •          Small application
+            •          Given a fully sorted array(ascending or descending), write a method to scramble the values
+            e.g. (7, 13, 13, 18, 29, 33)
+            •          Write the code with use-ability, debugging, and testing in mind.
+            */
+
+            Console.WriteLine("Question 1: Given a fully sorted array, write a method to scramble the values.");
+            Console.Write("Input 7 numbers in the array :\n");
+            int[] arr = new int[7];
+            int i;
+            for (i = 0; i < 7; i++)
+            {
+                Console.Write("number - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+            Console.WriteLine("Sorted Array:");
+            foreach(int number in arr)
+            {
+                Console.Write($"{number} ");
+            }
+            
+            Methods.Pause();
+
+            int[] RandomArray = Methods.Randomize(arr);
+            Console.WriteLine("Scrambled Array:");
+            foreach (int number in RandomArray)
+            {
+                Console.Write($"{number} ");
+            }
+            Methods.Pause();
+
+            /*Question 2(2, 3, build off each other)
+
+            * Consider the following style of abbreviation:
+            *(first letter) +(count of removed letters) +(last letter)
+            *For example:
+            *"internationalization"-> "i18n"
+            * "localization"-> "l10n"
+            * Write a function that given a word returns its abbreviation.*/
+
+            Console.WriteLine("Question 2: Write a function that given a word returns its abbreviation.");
+            string name;
+            Console.WriteLine("Enter your word: ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Your abbreviated word:");
+            Console.WriteLine(Methods.Abbreviate(name));
+            Methods.Pause();
+
+            /*Question 3
+
+            * Such abbreviations are not always unique, for example:
+            *   "accessibility" -> "a11y"
+            *   "automatically" -> "a11y"
+            * Given a dictionary (a list of words), write a function that takes a word and
+            * returns true if the abbreviation of the word is unique in the dictionary.
+            *
+            * For example, given a dictionary with the 4 words above:
+            *   "internationalization" -> true
+            *   "localization" -> true
+            *   "accessibility" -> false
+            *   "automatically" -> false
+            */
+
+            Console.WriteLine();
+            Console.WriteLine("Question 3: Given a dictionary, write a function that takes a word and returns true if the abbreviation of the word is unique in the dictionary.");
+            Console.WriteLine();
+            Dictionary<string, string> WordsAndAbbreviations = new Dictionary<string, string>();
+            string[] ArrayOfWords = { "accessibility", "automatically", "internationalization", "localization" };
+            foreach(string word in ArrayOfWords)
+            {
+                string ABWord = Methods.Abbreviate(word);
+                WordsAndAbbreviations.Add(word, ABWord);
+            }
+            Console.WriteLine("Dictionary of Words:");
+            foreach(KeyValuePair<string, string> kvp in WordsAndAbbreviations)
+            {
+                Console.WriteLine(kvp);
+            }
+            Methods.Pause();
+            Console.WriteLine();
+            string WordToCompare;
+            Console.WriteLine("Enter your word: ");
+            WordToCompare = Console.ReadLine();
+            Console.WriteLine();
+            Methods.CheckUniqueness(WordToCompare, WordsAndAbbreviations);
+            Methods.Pause();
+        }  
+    }
+}
